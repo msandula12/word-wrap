ce6036037b852ec6234765cc5d0922e7f78064f6
Mike Sandula <msandula@gmail.com>
2019-11-30 13:35:19 -0500

    feat: write word wrap function

--- spec/Wrapper.spec.js
+++ spec/Wrapper.spec.js
@@ -1,10 +1,27 @@
 const WordWrap = require('../src/Wrapper');
 
-const words = 'Amazingly few discotheques provide jukeboxes';
-
 describe('Word Wrap', () => {
-  it('should return words if no column number is provided', () => {
-    const result = WordWrap.wrap(words);
-    expect(result).toBe(words);
+  it('should return string unchanged if no column width is provided', () => {
+    expect(WordWrap.wrap('Please leave me as I am!')).toBe(
+      'Please leave me as I am!'
+    );
+  });
+  it('should return string unchanged if string is shorter than column width ', () => {
+    expect(WordWrap.wrap('Too short', 10)).toBe('Too short');
+  });
+  it('should break words at column', () => {
+    expect(WordWrap.wrap('abcdefghij', 3)).toBe('abc\ndef\nghi\nj');
+  });
+  it('should break words at word boundary', () => {
+    expect(WordWrap.wrap('Word word', 5)).toBe('Word\nword');
+  });
+  it('should break words after word boundary', () => {
+    expect(WordWrap.wrap('Word word', 6)).toBe('Word\nword');
+  });
+  it('should wrap before word boundary', () => {
+    expect(WordWrap.wrap('Word word', 3)).toBe('Wor\nd\nwor\nd');
+  });
+  it('should wrap just before word boundary', () => {
+    expect(WordWrap.wrap('Word word', 4)).toBe('Word\nword');
   });
 });
--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,7 +1,27 @@
 class Wrapper {
-  static wrap(words, numOfColumns) {
-    if (typeof numOfColumns !== 'number') {
-      return words;
+  static wrap(str, colWidth) {
+    if (typeof colWidth !== 'number' || str.length <= colWidth) {
+      return str;
+    }
+
+    // If substring contains empty space(s), break on the last one
+    const emptySpaceIndex = str.substring(0, colWidth).lastIndexOf(' ');
+    if (emptySpaceIndex !== -1) {
+      return `${str.substring(0, emptySpaceIndex)}\n${this.wrap(
+        str.substring(emptySpaceIndex + 1),
+        colWidth
+      )}`;
+      // New lines should not start with empty space
+    } else if (str.charAt(colWidth) === ' ') {
+      return `${str.substring(0, colWidth)}\n${this.wrap(
+        str.substring(colWidth + 1),
+        colWidth
+      )}`;
+    } else {
+      return `${str.substring(0, colWidth)}\n${this.wrap(
+        str.substring(colWidth),
+        colWidth
+      )}`;
     }
   }
 }
